20b8907397c55ae69e49199b8f954ef8eb5c0aa7
Vukasin Stancevic <stihilus@gmail.com>
2021-02-09 23:01:56 +0100

    version 0.1.1 - add assets

---
+++ preload.js
@@ -0,0 +1,29 @@
+// Loading all images from /assets folder
+function preload() {
+  bgd01 = loadImage("assets/bgd1.png");
+  bgd02 = loadImage("assets/bgd2.png");
+  bgd03 = loadImage("assets/bgd3.png");
+  bgd04 = loadImage("assets/bgd4.png");
+  bgd05 = loadImage("assets/bgd5.png");
+  bgd06 = loadImage("assets/bgd6.png");
+  bgd07 = loadImage("assets/bgd7.png");
+  bgd08 = loadImage("assets/bgd8.png");
+  bgd09 = loadImage("assets/bgd9.png");
+  bgd10 = loadImage("assets/bgd10.png");
+  
+  asset01 = loadImage("assets/asset1.png");
+  asset02 = loadImage("assets/asset2.png");
+  asset03 = loadImage("assets/asset3.png");
+  asset04 = loadImage("assets/asset4.png");
+  asset05 = loadImage("assets/asset5.png");
+  asset06 = loadImage("assets/asset6.png");
+  asset07 = loadImage("assets/asset7.png");
+  asset08 = loadImage("assets/asset8.png");
+  asset09 = loadImage("assets/asset9.png");
+  asset10 = loadImage("assets/asset10.png");
+  asset11 = loadImage("assets/asset11.png");
+  asset12 = loadImage("assets/asset12.png");
+  asset13 = loadImage("assets/asset13.png");
+  asset14 = loadImage("assets/asset14.png");
+  asset15 = loadImage("assets/asset15.png");
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 function setup() {
-  var canvasWidth = 500;
-  var canvasHeight = 500;
+  var canvasWidth = 1080;
+  var canvasHeight = 1080;
   var canvas = createCanvas(canvasWidth, canvasHeight);
   var address = ('waiting');
   canvas.parent('sketch-holder');
   background(215);
+
 }
 
 function showData() {
@@ -14,7 +15,7 @@ function showData() {
   var addressLenght = 41;
   var address = addressFromInput;
 
-  //split address
+  // split address
   let splitAddress = split(address, '');
 
   let value = [];
@@ -24,17 +25,51 @@ function showData() {
   for (i = 0; i < addressLenght; i++) {
     value[i] = splitAddress[i];
     valueUnchar[i] = unchar(value[i]);
-    valueMap[i] = map(valueUnchar[i], 48, 122, 0, 450);
+    valueMap[i] = map(valueUnchar[i], 48, 122, 0, 540);
   }
-  //background
+
+  // clear canvas
   background(215);
 
-  //drawing rectangles
-  for (i = 0; i < addressLenght; i++) {
-    
-    rect(valueMap[i], valueMap[i + 1], 55, 55);
-  }
+  // BACKGROUND 3. nubmer ------------------------
+  // background assets
+  var bgdAssets = [bgd01, bgd02, bgd03, bgd04, bgd05, bgd06, bgd07, bgd08, bgd09, bgd10];
+  // 3. nubmer picker BACKGROUND
+  // maping 3. nubmer to pick asset from bgdAssets[] //
+  var bgdBackground = Math.floor(map(valueMap[2], 0, 540, 0, 9));
+  //printing background image
+  image(bgdAssets[bgdBackground], 0, 0);
+
+
+  // Load assets to  ------------------------
+  var Assets = [asset01, asset02, asset03, asset04, asset05, asset06, asset07, asset08, asset09, asset10, asset11, asset12, asset13, asset14, asset15];
+  
+  // ASSET 01 ---- 4. number choose asset - 5. loc X - 6. loc Y
+  var assetPrint = Math.floor(map(valueMap[3], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[4], valueMap[5]);
+
+  // ASSETS 02 ---- 7. number - choose asset - 8. loc X - 9. loc Y
+  var assetPrint = Math.floor(map(valueMap[6], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[6], valueMap[7]);
+  
+  // ASSETS 03 ---- 10. number - choose asset - 11. loc X - 12. loc Y
+  var assetPrint = Math.floor(map(valueMap[9], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[10], valueMap[11]);
+  
+  // ASSETS 04 ---- 13. number - choose asset - 14. loc X - 15. loc Y
+  var assetPrint = Math.floor(map(valueMap[12], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[13], valueMap[14]);
+  
+  // ASSETS 05 ---- 16. number - choose asset - 17. loc X - 18. loc Y
+  var assetPrint = Math.floor(map(valueMap[15], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[16], valueMap[17]);
+  
+  // ASSETS 06 ---- 19. number - choose asset - 20. loc X - 21. loc Y
+  var assetPrint = Math.floor(map(valueMap[18], 0, 540, 0, 15));
+  image(Assets[assetPrint], valueMap[19], valueMap[20]);
 
 }
 
-function draw() {}
+function draw() {
+
+}
